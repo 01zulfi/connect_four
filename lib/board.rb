06e2dedbd308b266dc700ed62f6e85a593cf6e3c
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-08-21 18:08:53 +0500

    Create place_color public method & its tests * Add valid_column? & last_empty_row private methods

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,42 @@
 # frozen_string_literal: true
 
+require 'pry-byebug'
+
 # board
 class Board
   attr_reader :board
 
-  def initialize
-    @board = create_board
+  def initialize(board = create_board)
+    @board = board
+  end
+
+  def place_color(column, color)
+    return false unless valid_column?(column)
+
+    row = last_empty_row(column)
+
+    return false if row.nil?
+
+    board[row][column] = color
+    true
   end
 
   private
+
+  def valid_column?(column)
+    return false unless column.instance_of?(Integer)
+
+    (0..6).cover?(column)
+  end
+
+  def last_empty_row(column)
+    row_index = nil
+    board.each_with_index do |row, index|
+      row_index = index if row[column].nil?
+    end
+
+    row_index
+  end
 
   def create_board
     Array.new(6) { Array.new(7) }
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,4 +11,69 @@ describe Board do
       expect(board).to match_array(Array.new(6) { Array.new(7) })
     end
   end
+
+  describe '#place_color' do
+    subject(:board_color) { described_class.new }
+
+    context 'when non-integer column is given' do
+      it 'returns false' do
+        flag = board_color.place_color('a', 'red')
+        expect(flag).to be false
+      end
+    end
+
+    context 'when invalid column number is given' do
+      it 'returns false' do
+        flag = board_color.place_color(8, 'black')
+        expect(flag).to be false
+      end
+    end
+
+    context 'when valid column number is given' do
+      context 'when column is empty' do
+        it 'places color at the end of the column' do
+          board_color.place_color(0, 'red')
+          board = board_color.board
+          expect(board[5][0]).to eq('red')
+        end
+
+        it 'returns true' do
+          flag = board_color.place_color(0, 'red')
+          expect(flag).to be true
+        end
+      end
+
+      context 'when column is full' do
+        it 'returns false' do
+          board_color.place_color(0, 'red')
+          board_color.place_color(0, 'red')
+          board_color.place_color(0, 'red')
+          board_color.place_color(0, 'red')
+          board_color.place_color(0, 'red')
+          board_color.place_color(0, 'red')
+          flag = board_color.place_color(0, 'red')
+          expect(flag).to be false
+        end
+      end
+
+      context 'when column is slightly full/empty' do
+        before do
+          board_color.place_color(3, 'red')
+          board_color.place_color(3, 'red')
+          board_color.place_color(3, 'red')
+        end
+
+        it 'places color at the last empty row of the column' do
+          board_color.place_color(3, 'red')
+          board = board_color.board
+          expect(board[3][3]).to eq('red')
+        end
+
+        it 'returns true' do
+          flag = board_color.place_color(3, 'red')
+          expect(flag).to be true
+        end
+      end
+    end
+  end
 end
